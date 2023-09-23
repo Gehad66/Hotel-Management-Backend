2d20755f1dc7e1dbf08efdd74626197f782fd815
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-09-24 02:28:02 +0300

    docker updates

--- hotelier-api/src/config/config.js
+++ hotelier-api/src/config/config.js
@@ -2,7 +2,7 @@ const config = {
     'username': 'root',
     'password': 'secretG6',
     'database': 'hotels_db',
-    'db_host': process.env.MYSQL_HOST | 'localhost',
+    'db_host': process.env.MYSQL_HOST || 'localhost',
     'dialect': 'mysql',
   };
 module.exports = config;
