702e7f2b6c69dfd61fd1b4000a48dffa456f8781
Gehad66 <26491862+Gehad66@users.noreply.github.com>
2023-09-23 03:53:38 +0300

    Update index.js

--- booking-api/index.js
+++ booking-api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const BookingsRoute = require('./src/routes/bookings.routes');
 
 const app = express();
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send("About bookings");
 });
